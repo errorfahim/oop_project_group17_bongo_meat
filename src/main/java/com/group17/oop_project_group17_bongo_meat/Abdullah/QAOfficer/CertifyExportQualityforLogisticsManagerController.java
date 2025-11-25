45d6c6e01bb8a5eb1618d25b4843935a0be652c3
Alif-lucifer <alifyousufabdullah@gmail.com>
2025-11-26 04:48:46 +0600

    User 6 3 fxml designs

--- src/main/java/com/group17/oop_project_group17_bongo_meat/Abdullah/QAOfficer/CertifyExportQualityforLogisticsManagerController.java
+++ src/main/java/com/group17/oop_project_group17_bongo_meat/Abdullah/QAOfficer/CertifyExportQualityforLogisticsManagerController.java
@@ -1,7 +1,36 @@
 package com.group17.oop_project_group17_bongo_meat.Abdullah.QAOfficer;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.*;
+
 public class CertifyExportQualityforLogisticsManagerController
 {
     @javafx.fxml.FXML
+    private Label statusLabel;
+    @javafx.fxml.FXML
+    private ComboBox exportOrderCombo;
+    @javafx.fxml.FXML
+    private ListView documentList;
+    @javafx.fxml.FXML
+    private Button generateCertBtn;
+    @javafx.fxml.FXML
+    private Button reviewBtn;
+    @javafx.fxml.FXML
+    private Button downloadPdfBtn;
+
+    @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @Deprecated
+    public void handleGenerateCertificate(ActionEvent actionEvent) {
+    }
+
+    @Deprecated
+    public void handleViewDocumentation(ActionEvent actionEvent) {
+    }
+
+    @Deprecated
+    public void handleBack(ActionEvent actionEvent) {
+    }
+}
